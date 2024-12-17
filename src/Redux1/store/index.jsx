ef4d9f34da4242d01a691375584b36b89a97bce4
sw-kankeoi <kanekoi@solutionware.jp>
2024-12-17 13:54:32 +0900

    Redux1 UP,DOWNボタンを押すとエラーが発生する不具合の修正

--- src/Redux1/store/index.jsx
+++ src/Redux1/store/index.jsx
@@ -10,10 +10,12 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'INCREASE_COUNT':
             return {
+                ...state,
                 count: state.count + 1,
             };
         case 'DECREASE_COUNT':
             return {
+                ...state,
                 count: state.count - 1,
             };
         case 'GET_POST_DATA':
